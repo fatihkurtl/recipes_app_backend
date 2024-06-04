923eb8653a3d0f5eb95a697c690051ecabb292e3
fatihkurtl <kurtf061@gmail.com>
2024-06-04 15:14:44 +0300

    The code changes in this commit add the functionality to upload carousel images and drawer logo. It includes the implementation of the get_upload_carouesel_images and get_upload_drawer_logo functions in the dmin_crud.py file. These functions handle the uploading of carousel images and drawer logo files respectively.

--- router/admin/admin_crud.py
+++ router/admin/admin_crud.py
@@ -1,8 +1,95 @@
-from fastapi import HTTPException
+import os
+import shutil
+from fastapi import File, HTTPException, UploadFile
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
+from models.app import AppImages, Categories, Recipes
 from models.members import Members
 from schemas.members_schema import MemberBase
+
+
+
+def get_create_recipe(db: Session, title: str, title_en: str, description: str, description_en: str, thumbnail: UploadFile, save_count: int, popular: bool, active: bool, category_name: str, category_name_en: str):
+    try:
+        filename, file_extension = os.path.splitext(thumbnail.filename)
+        file_location = f"static/recipe_thumbnails/{thumbnail.filename}"
+        print("File Location:", file_location)
+        
+        # Klasörü oluştur
+        os.makedirs(os.path.dirname(file_location), exist_ok=True)
+
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(thumbnail.file, buffer)
+
+        category = db.query(Categories).filter(Categories.category_name == category_name).first()
+        if not category:
+            category = Categories(category_name=category_name, category_name_en=category_name_en)
+            db.add(category)
+            db.commit()
+            db.refresh(category)
+
+        recipe = Recipes(title=title, 
+                         title_en=title_en, 
+                         description=description, 
+                         description_en=description_en, 
+                         thumbnail_file=file_location, 
+                         save_count=save_count, 
+                         popular=popular, 
+                         active=active, 
+                         category_id=category.id)
+
+        if db.query(Recipes).filter(Recipes.title == recipe.title).first():
+            raise HTTPException(status_code=400, detail="Recipe title already exists")
+
+        db.add(recipe)
+        db.commit()
+        db.refresh(recipe)
+    except FileNotFoundError as e:
+        return JSONResponse(status_code=400, content={"message": str(e)})
+    return JSONResponse(status_code=200, content={"message": "Recipe added successfully"})
+
+
+def get_upload_carouesel_images(files: list[UploadFile], db: Session):
+    try:
+        file_locations = []
+        for file in files:
+            file_location = f"static/carousel_images/{file.filename}"
+            with open(file_location, "wb") as buffer:
+                shutil.copyfileobj(file.file, buffer)
+            file_locations.append(file_location)
+        
+        app_images = db.query(AppImages).first()
+        if not app_images:
+            app_images = AppImages()
+        for file_location in file_locations:
+            app_images.add_carousel_image(file_location)
+        db.add(app_images)
+        db.commit()
+        
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+    return JSONResponse(status_code=200, content={"message": "Carousel images uploaded successfully"})
+
+
+def get_upload_drawer_logo(file: UploadFile, db: Session):
+    try:
+        file_location = f"static/drawer_logo/{file.filename}"
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        
+        app_images = db.query(AppImages).first()
+        if not app_images:
+            app_images = AppImages()
+        app_images.drawer_header_logo = file_location
+        db.add(app_images)
+        db.commit()
+        
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+    return JSONResponse(status_code=200, content={"message": "Drawer logo uploaded successfully"})
 
 
 def get_all_members(db: Session, skip: int = 0, limit: int = 10):
--- router/admin/api.py
+++ router/admin/api.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from database.db import get_db
 from models.app import AppImages, Recipes, Categories
 from models.members import Members
-from router.admin.admin_crud import get_all_members
+from router.admin.admin_crud import get_all_members, get_create_recipe, get_upload_carouesel_images, get_upload_drawer_logo
 from schemas.members_schema import Member
 from .admin_auth import authenticate_admin, create_admin, create_admin_contact
 from schemas.admin_schema import LoginData, RegisterData, AdminContactData
@@ -37,46 +37,12 @@ async def contact_admin(contact_data: AdminContactData, db: Session = Depends(ge
 
 @router.post("/upload/drawer_logo")
 async def upload_drawer_logo(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    try:
-        file_location = f"static/drawer_logo/{file.filename}"
-        with open(file_location, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-        
-        app_images = db.query(AppImages).first()
-        if not app_images:
-            app_images = AppImages()
-        app_images.drawer_header_logo = file_location
-        db.add(app_images)
-        db.commit()
-        
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
-    return JSONResponse(status_code=200, content={"message": "Drawer logo uploaded successfully"})
+    return get_upload_drawer_logo(file, db)
 
 
 @router.post("/upload/carousel_images")
 async def upload_carousel_images(files: list[UploadFile] = File(...), db: Session = Depends(get_db)):
-    try:
-        file_locations = []
-        for file in files:
-            file_location = f"static/carousel_images/{file.filename}"
-            with open(file_location, "wb") as buffer:
-                shutil.copyfileobj(file.file, buffer)
-            file_locations.append(file_location)
-        
-        app_images = db.query(AppImages).first()
-        if not app_images:
-            app_images = AppImages()
-        for file_location in file_locations:
-            app_images.add_carousel_image(file_location)
-        db.add(app_images)
-        db.commit()
-        
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
-    return JSONResponse(status_code=200, content={"message": "Carousel images uploaded successfully"})
+    return get_upload_carouesel_images(files, db)
 
 
 @router.post("/add/recipe")
@@ -91,43 +57,7 @@ async def add_recipe(title: str = Form(...),
                      category_name: str = Form(...), 
                      category_name_en: str = Form(...), 
                      db: Session = Depends(get_db)):
-    try:
-        filename, file_extension = os.path.splitext(thumbnail.filename)
-        file_location = f"static/recipe_thumbnails/{thumbnail.filename}"
-        print("File Location:", file_location)
-        
-        # Klasörü oluştur
-        os.makedirs(os.path.dirname(file_location), exist_ok=True)
-
-        with open(file_location, "wb") as buffer:
-            shutil.copyfileobj(thumbnail.file, buffer)
-
-        category = db.query(Categories).filter(Categories.category_name == category_name).first()
-        if not category:
-            category = Categories(category_name=category_name, category_name_en=category_name_en)
-            db.add(category)
-            db.commit()
-            db.refresh(category)
-
-        recipe = Recipes(title=title, 
-                         title_en=title_en, 
-                         description=description, 
-                         description_en=description_en, 
-                         thumbnail_file=file_location, 
-                         save_count=save_count, 
-                         popular=popular, 
-                         active=active, 
-                         category_id=category.id)
-
-        if db.query(Recipes).filter(Recipes.title == recipe.title).first():
-            raise HTTPException(status_code=400, detail="Recipe title already exists")
-
-        db.add(recipe)
-        db.commit()
-        db.refresh(recipe)
-    except FileNotFoundError as e:
-        return JSONResponse(status_code=400, content={"message": str(e)})
-    return JSONResponse(status_code=200, content={"message": "Recipe added successfully"})
+    return get_create_recipe(db, title, title_en, description, description_en, thumbnail, save_count, popular, active, category_name, category_name_en)
 
 
 @router.get("/all/members", response_model=List[Member])
